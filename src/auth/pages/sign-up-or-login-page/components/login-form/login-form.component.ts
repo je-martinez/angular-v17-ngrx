f1657edb9a13d0ad808eb78e840691989307b796
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-12 11:52:28 -0600

    chore: implement form on login form

--- src/auth/pages/sign-up-or-login-page/components/login-form/login-form.component.ts
+++ src/auth/pages/sign-up-or-login-page/components/login-form/login-form.component.ts
@@ -1,18 +1,45 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SignUpOrLoginErrorForm } from 'src/auth/types';
+import { FormErrorMessageComponent } from 'src/shared/ui/components/form-error-message/form-error-message.component';
 
 @Component({
   selector: 'login-form',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgFor,
+    NgClass,
+    NgIf,
+    FormErrorMessageComponent
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
-  @Input() public signUpForm: FormGroup | undefined;
-  @Input() public emailErrors: string[] = [];
-  @Input() public passwordErrors: string[] = [];
-  @Output() public signUpWithEmailAndPassword = new EventEmitter();
-  @Output() public googleSignUp = new EventEmitter();
+  @Input() public loginForm: FormGroup | undefined;
+  @Input() public emailErrors: SignUpOrLoginErrorForm[] = [];
+  @Input() public passwordErrors: SignUpOrLoginErrorForm[] = [];
+  @Output() public loginWithEmailAndPassword = new EventEmitter();
+  @Output() public googleSignIn = new EventEmitter();
+
+  public get emailInputHasErrors() {
+    return this.emailErrors?.length > 0;
+  }
+
+  public get passwordInputHasErrors() {
+    return this.passwordErrors?.length > 0;
+  }
+
+  public onSubmit() {
+    this.loginWithEmailAndPassword.emit();
+  }
+
+  public onGoogleSignIn() {
+    this.googleSignIn.emit();
+  }
 }
