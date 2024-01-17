cf8d4feae03cbb72f0eae3ea983154b40ce72b4a
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-17 10:42:29 -0600

    chore: small changes on layout

--- src/layouts/main-view/components/main-view-layout/main-view-layout.component.ts
+++ src/layouts/main-view/components/main-view-layout/main-view-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Flowbite } from '@infrastructure/decorators/flowbite';
 import { AuthFacade } from '@store/modules/auth/auth.facade';
 
@@ -8,8 +8,17 @@ import { AuthFacade } from '@store/modules/auth/auth.facade';
   styleUrl: './main-view-layout.component.scss'
 })
 @Flowbite()
-export class MainViewLayoutComponent {
+export class MainViewLayoutComponent implements OnInit {
+  @ViewChild('mobileSideBarBtn', { static: false })
+  public mobileSideBarBtn: ElementRef<HTMLButtonElement> | undefined =
+    undefined;
   constructor(private readonly authFacade: AuthFacade) {}
+
+  ngOnInit(): void {
+    if (this.mobileSideBarBtn) {
+      this.mobileSideBarBtn.nativeElement.click();
+    }
+  }
 
   public get user() {
     return this.authFacade.user$;
