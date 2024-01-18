5a2ed29d4d2b5de75f53e058ddd8fceedf027e38
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-18 17:12:58 -0600

    test: unit testing layout service

--- src/layouts/auth/services/auth-layout.service.spec.ts
+++ src/layouts/auth/services/auth-layout.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthLayoutService } from './auth-layout.service';
 import { CommonModule } from '@angular/common';
+import { take } from 'rxjs';
 
 describe('AuthLayoutService', () => {
   let service: AuthLayoutService;
@@ -15,5 +16,14 @@ describe('AuthLayoutService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should change the value of the observable based on the value received in the method', () => {
+    for (const option of [true, false]) {
+      service.showTopProgressBar(option);
+      service.topProgressBar$.pipe(take(1)).subscribe((value) => {
+        expect(value).toBe(option);
+      });
+    }
   });
 });
