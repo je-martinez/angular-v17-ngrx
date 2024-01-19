a2468aa06a2f4a0851a6b9b447113448d8cde48e
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-19 09:12:17 -0600

    test: added mock users

---
+++ __mocks__/data/posts.mock.ts
@@ -0,0 +1,75 @@
+import { Post } from '@modules/home/types/content-wall.types';
+
+export const mockPosts: Post[] = [
+  {
+    userId: 1,
+    id: 1,
+    title:
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    createdAt: new Date('2024-01-03T07:56:27.187Z')
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    createdAt: new Date('2024-01-03T04:50:55.052Z')
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+    createdAt: new Date('2024-01-06T19:52:20.708Z')
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'eum et est occaecati',
+    body: 'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit',
+    createdAt: new Date('2023-12-29T21:28:00.333Z')
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'nesciunt quas odio',
+    body: 'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque',
+    createdAt: new Date('2024-01-09T14:10:47.730Z')
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: 'dolorem eum magni eos aperiam quia',
+    body: 'ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae',
+    createdAt: new Date('2023-12-26T21:45:36.486Z')
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: 'magnam facilis autem',
+    body: 'dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas',
+    createdAt: new Date('2023-12-22T20:15:50.957Z')
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: 'dolorem dolore est ipsam',
+    body: 'dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae',
+    createdAt: new Date('2024-01-17T14:38:27.172Z')
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: 'nesciunt iure omnis dolorem tempora et accusantium',
+    body: 'consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+    createdAt: new Date('2023-12-24T18:16:11.019Z')
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: 'optio molestias id quia eum',
+    body: 'quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error',
+    createdAt: new Date('2024-01-04T13:48:58.362Z')
+  }
+];
