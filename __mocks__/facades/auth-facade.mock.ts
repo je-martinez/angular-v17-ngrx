f4883131484396e472cc6f2fb9131b359107caa2
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-22 11:39:26 -0600

    test: wip working on coverage

--- __mocks__/facades/auth-facade.mock.ts
+++ __mocks__/facades/auth-facade.mock.ts
@@ -5,12 +5,24 @@ import { of } from 'rxjs';
 export const generateMockAuthFacade = (emptyUser = false): AuthFacade => {
   const facade = jasmine.createSpyObj<AuthFacade>(
     'AuthFacade',
-    ['signOut'],
+    [
+      'signInWGoogle',
+      'signInWGithub',
+      'signUpWEmailAndPassword',
+      'signInWEmailAndPassword',
+      'signOut',
+      'recoveUserFromStorage'
+    ],
     ['user$']
   );
   Object.defineProperty(facade, 'user$', {
     value: of(emptyUser ? null : mockLoggedUser)
   });
+  facade.signInWGoogle.and.returnValue();
+  facade.signInWGithub.and.returnValue();
+  facade.signUpWEmailAndPassword.and.returnValue();
+  facade.signInWEmailAndPassword.and.returnValue();
   facade.signOut.and.returnValue();
+  facade.recoveUserFromStorage.and.returnValue();
   return facade;
 };
