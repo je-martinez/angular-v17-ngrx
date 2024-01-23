25c47b0ff743c7208d53b1dede90f28963d6aee4
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-01-23 16:33:37 -0600

    test: make mock for state

--- __mocks__/store/state.mock.ts
+++ __mocks__/store/state.mock.ts
@@ -1,13 +1,26 @@
-import { mockLoggedUser } from '@mocks/data/users.mock';
+import { mockComments } from '@mocks/data/comments.mock';
+import { mockPosts } from '@mocks/data/posts.mock';
+import { mockLoggedUser, mockUsers } from '@mocks/data/users.mock';
 import {
   AuthState,
   initialState as AuthInitialState
 } from '@store/modules/auth/auth.reducer';
+import {
+  ContentState,
+  initialState as ContentInitialState
+} from '@store/modules/content/content.reducer';
 import { RootState } from '@store/modules/types/store.types';
 
-export const createAuthState = (): RootState => ({
+export const createRootState = (): RootState => ({
   auth: {
     ...AuthInitialState,
     user: mockLoggedUser
-  } as AuthState
+  } as AuthState,
+  content: {
+    ...ContentInitialState,
+    posts: [...mockPosts],
+    users: [...mockUsers],
+    comments: [...mockComments],
+    contentId: mockPosts[0].id
+  } as ContentState
 });
--- src/store/modules/auth/auth.selectors.spec.ts
+++ src/store/modules/auth/auth.selectors.spec.ts
@@ -1,15 +1,15 @@
-import { createAuthState } from '@mocks/store/state.mock';
+import { createRootState } from '@mocks/store/state.mock';
 import { selectAuthState, selectUser } from './auth.selectors';
 
 describe('Auth Selectors', () => {
   it('should select the feature state', () => {
-    const state = createAuthState();
+    const state = createRootState();
     const result = selectAuthState(state);
     expect(result).toEqual(state.auth);
   });
 
   it('should select the current user from state', () => {
-    const state = createAuthState();
+    const state = createRootState();
     const result = selectUser(state);
     expect(result).toEqual(state.auth.user);
   });
